306ce7397900b468af05ee2b483f646e6b120d84
Adrian Christian Santos <adrian.crsantos@gmail.com>
2023-08-06 14:17:52 +0800

    Fixed icon

--- config.def.h
+++ config.def.h
@@ -62,14 +62,14 @@ static const char unknown_str[] = "n/a";
  * wifi_essid          WiFi ESSID                      interface name (wlan0)
  */
 static const struct arg args[] = {
-	/* function format          argument */
-	//{ datetime, "%s",           "%F %T" },
-	//{ wifi_perc, "W: (%3s%% on ", "wlp8s0" },
+    /* function format          argument */
+    //{ datetime, "%s",           "%F %T" },
+    //{ wifi_perc, "W: (%3s%% on ", "wlp8s0" },
     //{ netspeed_rx, "%sB/s  ", "enp0s3" },
-	{ run_command, " |  %2s | ", "pamixer --get-volume" },
-	{ cpu_perc, " %s%% | ", NULL	      },
-	{ ram_perc, " %s%% | ", NULL	      },
-	{ datetime, "%s",           "%a %b %d | %r " },
-	{ battery_state, "%s",           "BAT0" },
-	{ battery_perc, "%s%%",           "BAT0" },
+    {run_command, " |  %2s | ", "pamixer --get-volume"},
+    {cpu_perc, " %s%% | ", NULL},
+    {ram_perc, "󰍛 %s%% | ", NULL},
+    {datetime, "%s", "%a %b %d | %r "},
+    {battery_state, "%s", "BAT0"},
+    {battery_perc, "%s%%", "BAT0"},
 };
